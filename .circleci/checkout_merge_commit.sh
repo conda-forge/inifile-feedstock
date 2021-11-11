cea095e29b715a018c638eb5a7e06319edbc1ee7
antonio <a.cavallo@cavallinux.eu>
2021-11-11 10:14:46 -0500

    debug

--- .circleci/checkout_merge_commit.sh
+++ .circleci/checkout_merge_commit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+function info { echo "+ $@" >&2; }
 
 # Update PR refs for testing.
 if [[ -n "${CIRCLE_PR_NUMBER}" ]]
@@ -11,17 +11,20 @@ fi
 # Retrieve the refs.
 if [[ -n "${CIRCLE_PR_NUMBER}" ]]
 then
+    info "fetch refs: git fetch -u origin ${FETCH_REFS}"
     git fetch -u origin ${FETCH_REFS}
 fi
 
 # Checkout the PR merge ref.
 if [[ -n "${CIRCLE_PR_NUMBER}" ]]
 then
+    info "checkout: git checkout -qf pr/${CIRCLE_PR_NUMBER}/merge"
     git checkout -qf "pr/${CIRCLE_PR_NUMBER}/merge"
 fi
 
 # Check for merge conflicts.
 if [[ -n "${CIRCLE_PR_NUMBER}" ]]
 then
+    info "branching" 
     git branch --merged | grep "pr/${CIRCLE_PR_NUMBER}/head" > /dev/null
 fi
